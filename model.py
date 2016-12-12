73c335b7145c7e656f3ea8ce56b3d15735237fa5
Benjamin Bolte <bkbolte18@gmail.com>
2016-12-11 17:36:44 -0800

    added first file :poop:

---
+++ model.py
@@ -0,0 +1,9 @@
+"""Defines the components for the model."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+if __name__ == '__main__':
+    print('Hello world!')
